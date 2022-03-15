80f266209ddae1fbbe6dda26ce390fac282b284a
Alexis Castellanos <castellanos.alexis@hotmail.com>
2022-03-15 02:00:31 -0600

    styles to loginscreen

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -13,14 +13,18 @@ import { AuthRouter } from './AuthRouter';
 
 export const AppRouter = () => {
    return (
-      <Router>
-         <div>
-            <Switch>
-               <Route path="/auth" component={AuthRouter} />
-               <Route exact path="/" component={JournalScreen} />
-               <Redirect to="/auth/register" />
-            </Switch>
+      <div className="auth__main">
+         <div className="auth__box-container">
+            <Router>
+               <div>
+                  <Switch>
+                     <Route path="/auth" component={AuthRouter} />
+                     <Route exact path="/" component={JournalScreen} />
+                     <Redirect to="/auth/register" />
+                  </Switch>
+               </div>
+            </Router>
          </div>
-      </Router>
+      </div>
    )
 }
