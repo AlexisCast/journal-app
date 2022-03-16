131f180a29bf1d9fc209ac6c4760a09609636117
Alexis Castellanos <castellanos.alexis@hotmail.com>
2022-03-16 01:12:23 -0600

    structure and design of HomeScreen

--- src/components/notes/NotesScreen.js
+++ src/components/notes/NotesScreen.js
@@ -5,6 +5,25 @@ export const NotesScreen = () => {
    return (
       <div className='notes__main-content'>
          <NotesAppBar />
+         <div className='notes__content'>
+            <input
+               type="text"
+               placeholder="some awesome title"
+               className='notes___title-input'
+               autoComplete='off'
+            />
+            <textarea
+               placeholder="What happened today"
+               className='notes_textarea'
+            >
+            </textarea>
+            <div className='notes__image'>
+               <img
+                  src="https://thumbs.dreamstime.com/z/april-baby-grogu-yoda-stands-colorful-toys-realistic-toy-blurred-background-high-quality-photo-216079590.jpg"
+                  alt='imagen'
+               />
+            </div>
+         </div>
       </div>
    )
 }
