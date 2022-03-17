7a29b1a988e5202556141c7621036995735234bc
Alexis Castellanos <castellanos.alexis@hotmail.com>
2022-03-17 04:45:27 -0600

    configure firebase, google sign-in and set up .env variables

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,30 @@
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+   apiKey: process.env.REACT_APP_FIREBASE_apiKey,
+   authDomain: process.env.REACT_APP_FIREBASE_authDomain,
+   projectId: process.env.REACT_APP_FIREBASE_projectId,
+   storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
+   messagingSenderId:process.env.REACT_APP_FIREBASE_messagingSenderId,
+   appId: process.env.REACT_APP_FIREBASE_appId,
+   measurementId: process.env.REACT_APP_FIREBASE_measurementId
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const googleAuthProvider = new GoogleAuthProvider();
+
+export {
+   db,
+   googleAuthProvider,
+   app
+}
+
+
+
