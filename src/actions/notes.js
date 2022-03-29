e01432a9629bb76bfc2971c8a434fbd36f9ba708
Alexis Castellanos <castellanos.alexis@hotmail.com>
2022-03-29 02:45:57 -0600

    create new note

---
+++ src/actions/notes.js
@@ -0,0 +1,19 @@
+import { db } from '../firebase/firebase-config';
+import { collection, addDoc } from "firebase/firestore";
+
+
+export const startNewNote = () => {
+   return async (dispatch, getState) => {
+      const { uid } = getState().auth;
+      console.log(uid);
+
+      const newNote = {
+         title: '',
+         body: '',
+         date: new Date().getTime()
+      }
+
+      const doc = await addDoc(collection(db, `${uid}`, "journal/notes"), newNote);
+      console.log("Document written with ID: ", doc);
+   }
+}
